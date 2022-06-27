21fca1f8eb98231c5cf84c06a8903b06b2e2d202
himanshu@webtheuniverse.com <himanshu@webtheuniverse.com>
2022-06-27 14:53:19 +0530

    complete model window task

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const showModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+const addHidden = function () {
+    modal.classList.add('hidden');
+    overLay.classList.add('hidden');
+}
+// METHOD-1 close-model
+closeModal.addEventListener('click', () => {
+     addHidden();
+});
+
+// METHOD-2 close-model
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      addHidden();
+    }
+});
+
+// METHOD-1 show-modal
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', () => {
+       modal.classList.remove('hidden');
+       overLay.classList.remove('hidden');
+  });
+}
+
+// METHOD-2 show-modal
+// showModal.forEach(Modal => {
+//     Modal.addEventListener('click', () => {
+//         modal.classList.remove('hidden');
+//         overLay.classList.remove("hidden");
+//     })   
+// })
